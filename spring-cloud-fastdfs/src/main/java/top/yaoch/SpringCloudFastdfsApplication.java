3ea35b5c881865bf2f62bf7c75c798f4db25850c
fatYch <897141839@qq.com>
2020-07-26 01:07:10 +0800

    feat: fastfds服务器

---
+++ spring-cloud-fastdfs/src/main/java/top/yaoch/SpringCloudFastdfsApplication.java
@@ -0,0 +1,13 @@
+package top.yaoch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudFastdfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudFastdfsApplication.class, args);
+    }
+
+}
